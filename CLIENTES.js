7f29cd55085603f8c58759fe220fb44557506e3e
DorlixitoG <vbkevin.iesanpablo@gmail.com>
2023-10-21 00:11:54 -0500

    añadi clientes

---
+++ CLIENTES.js
@@ -0,0 +1,435 @@
+use('Supermercado')
+
+
+//CRUD
+
+// db.Clientes.insertOne({
+//     "Nombre": "Sofía",
+//     "Telefono": "4112345678",
+//     "Direccion": "Calle 28 #23-12",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "carne",
+//             "Cantidad": 5
+//         },
+//         {
+//             "Producto": "papas",
+//             "Cantidad": 4
+//         },
+//         {
+//             "Producto": "cerveza",
+//             "Cantidad": 6
+//         },
+//         {
+//             "Producto": "abichuela",
+//             "Cantidad": 3
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Carlos",
+//     "Telefono": "5551234567",
+//     "Direccion": "Avenida Principal",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "leche",
+//             "Cantidad": 2
+//         },
+//         {
+//             "Producto": "pan",
+//             "Cantidad": 3
+//         },
+//         {
+//             "Producto": "huevos",
+//             "Cantidad": 12
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "María",
+//     "Telefono": "1239876543",
+//     "Direccion": "Calle 5",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "arroz",
+//             "Cantidad": 8
+//         },
+//         {
+//             "Producto": "frijoles",
+//             "Cantidad": 4
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Juan",
+//     "Telefono": "9876543210",
+//     "Direccion": "Avenida Central",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "pollo",
+//             "Cantidad": 3
+//         },
+//         {
+//             "Producto": "papas",
+//             "Cantidad": 5
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Ana",
+//     "Telefono": "3338887777",
+//     "Direccion": "Calle 12",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "manzanas",
+//             "Cantidad": 10
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Luis",
+//     "Telefono": "5555555555",
+//     "Direccion": "Avenida Norte",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "agua",
+//             "Cantidad": 24
+//         },
+//         {
+//             "Producto": "galletas",
+//             "Cantidad": 2
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Isabel",
+//     "Telefono": "1234567890",
+//     "Direccion": "Calle Sur",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "café",
+//             "Cantidad": 1
+//         },
+//         {
+//             "Producto": "té",
+//             "Cantidad": 3
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Pedro",
+//     "Telefono": "9990001111",
+//     "Direccion": "Avenida Oeste",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "pan",
+//             "Cantidad": 2
+//         },
+//         {
+//             "Producto": "leche",
+//             "Cantidad": 3
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Lorena",
+//     "Telefono": "7771112222",
+//     "Direccion": "Calle Este",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "yogur",
+//             "Cantidad": 6
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Roberto",
+//     "Telefono": "5557778888",
+//     "Direccion": "Avenida 10",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "manzanas",
+//             "Cantidad": 5
+//         },
+//         {
+//             "Producto": "peras",
+//             "Cantidad": 4
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Laura",
+//     "Telefono": "1110009999",
+//     "Direccion": "Calle 7",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "pescado",
+//             "Cantidad": 2
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Fernando",
+//     "Telefono": "9998887777",
+//     "Direccion": "Avenida 4",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "manzanas",
+//             "Cantidad": 3
+//         },
+//         {
+//             "Producto": "uvas",
+//             "Cantidad": 2
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Patricia",
+//     "Telefono": "1231231234",
+//     "Direccion": "Calle 9",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "carne",
+//             "Cantidad": 3
+//         },
+//         {
+//             "Producto": "pollo",
+//             "Cantidad": 2
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Eduardo",
+//     "Telefono": "7779991111",
+//     "Direccion": "Avenida 6",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "papas",
+//             "Cantidad": 6
+//         },
+//         {
+//             "Producto": "cebollas",
+//             "Cantidad": 4
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Carmen",
+//     "Telefono": "5553331111",
+//     "Direccion": "Calle 14",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "agua",
+//             "Cantidad": 12
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Hugo",
+//     "Telefono": "1234567890",
+//     "Direccion": "Avenida 2",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "leche",
+//             "Cantidad": 2
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Raquel",
+//     "Telefono": "9997775555",
+//     "Direccion": "Calle 3",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "huevos",
+//             "Cantidad": 18
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Miguel",
+//     "Telefono": "8888888888",
+//     "Direccion": "Avenida 1",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "arroz",
+//             "Cantidad": 4
+//         },
+//         {
+//             "Producto": "frijoles",
+//             "Cantidad": 4
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Lucía",
+//     "Telefono": "7777777777",
+//     "Direccion": "Calle 11",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "manzanas",
+//             "Cantidad": 6
+//         },
+//         {
+//             "Producto": "naranjas",
+//             "Cantidad": 8
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Alejandro",
+//     "Telefono": "6666666666",
+//     "Direccion": "Avenida 3",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "papas",
+//             "Cantidad": 5
+//         },
+//         {
+//             "Producto": "cerveza",
+//             "Cantidad": 12
+//         }
+//     ]
+// });
+
+// db.Clientes.insertOne({
+//     "Nombre": "Cecilia",
+//     "Telefono": "5555555555",
+//     "Direccion": "Calle 10",
+//     "Productos_Comprados": [
+//         {
+//             "Producto": "leche",
+//             "Cantidad": 4
+//         },
+//         {
+//             "Producto": "pan",
+//             "Cantidad": 2
+//         }
+//     ]
+// });
+
+
+// db.Clientes.find({ "Productos_Comprados.Cantidad": {$gte:2} });
+
+// db.Clientes.updateOne(
+//     { Nombre: "Alejandro", "Productos_Comprados.Producto": "cerveza" },
+//     { $set: { "Productos_Comprados.$.Cantidad": 3000 } }
+//   );
+
+  
+// db.Clientes.remove({ "Productos_Comprados.Producto": "pan" });
+
+//indices
+
+// db.Clientes.createIndex({ Nombre: 1 });
+      
+
+// db.Clientes.find({}, { Nombre: 1 }).sort({ Nombre: 1 });
+
+// db.Clientes.createIndex({ Nombre: 1, Productos_Comprados: 1 });
+
+
+// db.Clientes.find({ Nombre: "Lucia", "Productos_Comprados.Cantidad": 8 });
+
+
+//Agregaciones
+
+// db.Clientes.aggregate([
+//     {$match: { "Nombre": "Cecilia","Productos_Comprados.Cantidad": 4  }}
+//   ]);
+  
+
+// db.Clientes.aggregate([
+//   { $project: { _id: 0, Nombre: 1, Telefono: 1, Direccion: 1 } },
+// ]);
+
+
+// db.Clientes.aggregate([
+//   { $group: { _id: "$Nombre", totaldocs: { $sum: 1 } } },
+// ]);
+
+
+// db.Clientes.aggregate([
+//   { $group: { _id: "$Nombre", totaldocs: { $sum: 1 } } },
+//   { $out: "aggResults" },
+// ]);
+// db.aggResults.find();
+
+
+//  db.Clientes.aggregate([
+//       {
+//         $match: { Nombre: "Alejandro" },
+//       },
+//       { $unwind: "$Productos_Comprados" },
+//     ]);
+    
+
+
+// db.Clientes.aggregate([
+//   { $match: { Nombre: "Alejandro" } },
+//   { $unwind: "$Productos_Comprados" },
+//   { $project: { _id: 0, "students.year": 1, "Productos_Comprados.Producto": 1 } },
+//   { $sort: { "Productos_Comprados.Producto": -1 } },
+// ]);
+
+
+// db.Clientes.aggregate([
+//     { $match: { Nombre: "Alejandro" } },
+//     { $unwind: "$Productos_Comprados" },
+//     { $project: { _id: 0, "students.year": 1, "Productos_Comprados.Producto": 1 } },
+//     { $sort: { "Productos_Comprados.Producto": -1 } },
+//     { $limit: 2 },
+//   ]);
+
+
+// db.Clientes.aggregate([
+//     {$match: { Nombre: "Cecilia" } },
+//     {$addFields: { TotalCompras: {  $sum: "$Productos_Comprados.Cantidad" } }}
+//   ]);
+  
+
+// db.Clientes.aggregate([
+//   { $unwind: "$Productos_Comprados"},
+//   { $count: "total_documents" },
+// ]);
+
+
+// db.Clientes.aggregate([
+//     { $match: { Nombre: "Alejandro" } },
+//     { $project: { _id: 0, Nombre: 1 } },
+//     {
+//       $lookup: {
+//         from: "Productos",
+//         localField: "Nombre_Product",
+//         foreignField: "Nombre_Product", 
+//         as: "Productos_Comprados",
+//       },
+//     }
+//   ]);
+  
+
+// db.Clientes.aggregate([{ $sortByCount: "$Cantidad" }]);
+
+// db.Clientes.aggregate([
+//   { $unwind: "$Productos_Comprados" },
+//   { $group: { _id: "$Nombre", totalProduct: { $sum: "$Productos_Comprados.Cantidad" } } },
+// ]);
